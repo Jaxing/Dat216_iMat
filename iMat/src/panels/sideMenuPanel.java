08a1b626e350e1e2e8147bf4dd3233b371bc7aa2
ErikKarlkvist <erikkarlkvist95@gmail.com>
2015-03-04 19:02:37 +0100

    error fix

--- iMat/src/panels/sideMenuPanel.java
+++ iMat/src/panels/sideMenuPanel.java
@@ -2,13 +2,7 @@ package panels;
 
 
 
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-//package panels;
 import customBackend.EventHandler;
 import customBackend.EventListener;
 import javax.swing.ImageIcon;
